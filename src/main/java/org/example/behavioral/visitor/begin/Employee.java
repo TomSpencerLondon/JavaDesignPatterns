02d65eb1c6e004d7f655c93f6bf8bb37408fd697
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2024-10-25 09:05:36 +0100

    add printVisitor

---
+++ src/main/java/org/example/behavioral/visitor/begin/Employee.java
@@ -0,0 +1,16 @@
+package org.example.behavioral.visitor.begin;
+
+import java.util.Collection;
+
+public interface Employee {
+	
+	int getPerformanceRating();
+	
+	void setPerformanceRating(int rating);
+	
+	Collection<Employee> getDirectReports();
+
+	int getEmployeeId();
+
+	void accept(Visitor visitor);
+}
